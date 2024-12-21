b982ccd10ce56d6916dad2617fa8cdd729ac7db7
ptrnk0 <vlad.ptrnko@gmail.com>
2024-12-21 16:59:32 +0200

    Update ContactForm styles and validation messages

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -3,8 +3,14 @@ import * as Yup from "yup";
 import css from "./ContactForm.module.css";
 
 const contactFormSchema = Yup.object().shape({
-	name: Yup.string().min(3).max(50).required(),
-	number: Yup.string().min(3).max(50).required(),
+	name: Yup.string()
+		.min(3, "Name must be at least 3 characters")
+		.max(50, "Name must be at most 50 characters")
+		.required("Name is required"),
+	number: Yup.string()
+		.min(3, "Number must be at least 3 characters")
+		.max(50, "Number must be at most 50 characters")
+		.required("Number is required"),
 });
 
 const ContactForm = ({ onAddContact }) => {
@@ -20,18 +26,40 @@ const ContactForm = ({ onAddContact }) => {
 			onSubmit={handleSubmit}
 			validationSchema={contactFormSchema}
 		>
-			<Form>
-				<label>
-					<span>Name</span>
-					<Field type="text" name="name"></Field>
-					<ErrorMessage name="name" component="span" />
-				</label>
-				<label>
-					<span>Number</span>
-					<Field type="text" name="number"></Field>
-					<ErrorMessage name="number" component="span" />
-				</label>
-				<button type="submit">Add contact</button>
+			<Form className={css.contactForm}>
+				<div className={css.contactFieldContainer}>
+					<div>
+						<Field
+							type="text"
+							name="name"
+							className={css.contactLabelField}
+							placeholder="Name"
+						></Field>
+						<ErrorMessage
+							name="name"
+							component="span"
+							className={css.contactError}
+						/>
+					</div>
+
+					<div>
+						<Field
+							type="text"
+							name="number"
+							className={css.contactLabelField}
+							placeholder="Number"
+						></Field>
+						<ErrorMessage
+							name="number"
+							component="span"
+							className={css.contactError}
+						/>
+					</div>
+				</div>
+
+				<button type="submit" className={css.contactBtn}>
+					Add contact
+				</button>
 			</Form>
 		</Formik>
 	);
